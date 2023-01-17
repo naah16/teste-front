e42991638e79143cd400d48212e54c7f9f4079d7
naah16 <naaraholiveira@gmail.com>
2023-01-17 11:36:49 -0300

    first commit

---
+++ js/owl/setup.js
@@ -0,0 +1,15 @@
+$('.owl-carousel').owlCarousel({
+    loop:false,
+    nav:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+})
